4d73c3dc1a7754dca7203c41cc56bda5267a7eab
unknown <brunofurquimngnl@gmail.com>
2024-09-23 11:36:36 -0300

    wip

---
+++ AcompanhamentoDeEgresso/Controller/UserController.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserController {
+    private List<User> users;
+
+    public UserController() {
+        users = new ArrayList<>();
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+        System.out.println("User added: " + user.getName());
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+}
---
+++ AcompanhamentoDeEgresso/Model/Administrator.java
@@ -0,0 +1,6 @@
+public class Administrator extends User {
+
+    public Administrator(String name, String email, String password) {
+        super(name, email, password);
+    }
+}
