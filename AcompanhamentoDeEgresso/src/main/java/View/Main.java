9665492246bbb83f7c11373c2f7233915167335e
Rafael Rosseto <rafaelbassirosseto@gmail.com>
2024-11-05 12:48:34 -0300

    feat: update interface

--- AcompanhamentoDeEgresso/src/main/java/View/Main.java
+++ AcompanhamentoDeEgresso/src/main/java/View/Main.java
@@ -1,27 +1,28 @@
 package  View;
 
 import Controller.UserController;
-import Model.Egress;
 import Model.User;
 import Model.UserSerializable;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         UserController userController = new UserController();
         UserSerializable serializable = new UserSerializable();
 
-        User user1 = new User("William", "william@unesp.com", "senha123");
-        Egress egress1 = new Egress("Karol", "karol@unesp.com", "senha456", new Date(), new Date(), new Date(),
-                new ArrayList<>(), "public");
-
-        serializable.addUser(user1, userController.getUsers());
+        /*
+        Egress egress1 = new Egress("Karol", "karol@unesp.com", "senha456", new Date(), new Date(), new Date(), new ArrayList<>(), "public");
         serializable.addUser(egress1, userController.getUsers());
-
         User user2 = new User("rogerio", "karol@unesp.com", "senha789");
         serializable.addUser(user2, userController.getUsers());
+        */
         
+        List<User> users = new ArrayList<>();
+        User admin = new User("admin", "admin", "admin");
+        serializable.addUser(admin, users);
+        LoginScreen loginScreen = new LoginScreen(users);
+        loginScreen.setVisible(true);
         MainFrame myFrame = new MainFrame();
         myFrame.init();
     }
